4fc667ef3747728e40cadb6738ad527afabc51f6
Janno Kusman <janno.kusman@cyber.ee>
2024-05-22 23:54:11 +0300

    RM-3196: git push branch

--- make_release.sh
+++ make_release.sh
@@ -8,7 +8,7 @@
 # docker deploy
 
 #export GIT_REMOTE=gitlab.ext
-export CDOC2_SERVER_VER=$(mvn -f cdoc2-server help:evaluate -Dexpression=project.version -q -DforceStdout)
+CDOC2_SERVER_VER=$(mvn -f cdoc2-server help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 GIT_BRANCH=$(git branch --show-current)
 GIT_REMOTE=$(git config --get-regexp "branch\.$GIT_BRANCH\.remote" | sed -e "s/^.* //")
