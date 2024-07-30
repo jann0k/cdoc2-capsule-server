4a5affdc8ba4d0b82bcdf5f42741baf9309adf39
Janno Kusman <janno.kusman@cyber.ee>
2024-07-30 18:27:20 +0300

    RM-3487: Create cdoc2-server-openapi module, that generates servers stubs from ee.cyber.cdoc2.openapi:cdco2-key-capsules-openapi:yaml maven dependency. Update put-server to use cdoc2-server-openapi instead of cdoc2-openapi (to be moved into separte repo)

--- prepare_release.sh
+++ prepare_release.sh
@@ -53,6 +53,10 @@ fi
 #clean up local maven repo
 #mvn -f cdoc2-server dependency:purge-local-repository -Dexclude=ee.cyber.cdoc2:cdoc2-lib,ee.cyber.cdoc2:cdoc2-key-capsules-openapi -Dinclude=ee.cyber.cdoc2:cdoc2-server,ee.cyber.cdoc2:cdoc2-common-server,ee.cyber.cdoc2:cdoc2-server-db,ee.cyber.cdoc2:cdoc2-get-server,ee.cyber.cdoc2:cdoc2-put-server -DresolutionFuzziness=artifactId
 
+#https://github.com/amoschov/maven-dependency-plugin/blob/master/src/site/apt/examples/purging-local-repository.apt.vm
+# mvn -P 'github,!gitlab.ext' dependency:purge-local-repository -DactTransitively=false -DreResolve=false -DmanualInclude=ee.cyber.cdoc2.openapi:cdoc2-key-capsules-openapi -Dverbose=true
+# mvn -P 'github,!gitlab.ext' dependency:purge-local-repository -DmanualInclude=ee.cyber.cdoc2.openapi:cdoc2-key-capsules-openapi
+
 # replace module -SNAPSHOT version with release version (non-SNAPSHOT)
 mvn -f cdoc2-shared-crypto versions:set -DremoveSnapshot
 # build and install into local maven package repository
